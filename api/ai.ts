d5ed696c587c5b603030c205caa312a846f5d5d8
Bao Huynh <bao.huynh@mesoneer.io>
2024-11-23 17:04:11 +0700

    change

--- api/ai.ts
+++ api/ai.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AiResponse } from 'interfaces/ai.type';
 
 export const AiCommunicateApi = async (formData: FormData): Promise<AiResponse> => {
-    const response = await axios.post<AiResponse>(`http://showcase-api.memora.vn/audio/process`, formData, {
+    const response = await axios.post<AiResponse>(`https://showcase-api.memora.vn/audio/process`, formData, {
     // const response = await axios.post(`http://localhost:4000/audio/process`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data', 
